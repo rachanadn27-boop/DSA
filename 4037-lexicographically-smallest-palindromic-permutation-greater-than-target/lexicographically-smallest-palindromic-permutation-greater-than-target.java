1f722738cdd89489d19e1c0f049f6a3add010bfa
Rachana D N <rachanadn27@gmail.com>
2026-08-28 17:23:31 +0530

    Time: 6 ms (76.19%) | Memory: 47.3 MB (42.86%) - LeetSync

--- 4037-lexicographically-smallest-palindromic-permutation-greater-than-target/lexicographically-smallest-palindromic-permutation-greater-than-target.java
+++ 4037-lexicographically-smallest-palindromic-permutation-greater-than-target/lexicographically-smallest-palindromic-permutation-greater-than-target.java
@@ -12,7 +12,6 @@ class Solution {
                 freq[i]--;
             }
         }
-
         int sz = s.length();
         int half = sz / 2;
         for (int i = 0; i < half; i++)
@@ -28,7 +27,6 @@ class Solution {
             if (tail.compareTo(target.substring(half)) > 0)
                 return head + tail;
         }
-
         for (int i = half - 1; i >= 0; i--) {
             char w = target.charAt(i);
             freq[w - 'a'] += 2;
