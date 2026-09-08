c789415177081b6553de86cbdfc9f07cbed5cc0b
Rachana D N <rachanadn27@gmail.com>
2026-09-08 21:20:40 +0530

    Time: 26 ms (13.89%) | Memory: 47.3 MB (98.72%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,27 +1,29 @@
-public class Solution {
+class Solution {
+    public static  int max_value(int piles[]){
+        int max=Integer.MIN_VALUE;
+        for (int i=0;i< piles.length;i++){
+            max=Math.max(piles[i],max);
+        }
+        return max;
+    }
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;  
-        int right = Arrays.stream(piles).max().getAsInt();
-        int ans = right;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canFinish(piles, h, mid)) {
-                ans = mid;     
-                right = mid - 1;
-            } else {
-                left = mid + 1; 
-            }
+        int n = piles.length;
+        int ans = Integer.MIN_VALUE;
+            int low = 1;
+            int high = max_value(piles);
+        while (low <=high){
+            int mid = low + (high-low)/2;
+             int value = islessthan(piles,mid,h);
+             if (value<=h) { ans = mid;high = mid-1;}
+             else low = mid + 1;
         }
         return ans;
     }
-
-    public boolean canFinish(int[] piles, int h, int k) {
-        long hours = 0;
-        for (int pile : piles) {
-            hours += pile / k;
-            if (pile % k != 0) hours++;
+    public static int  islessthan(int piles[],int mid,int h){
+        int count = 0;
+        for (int i = 0;i< piles.length; i++){
+              count+=Math.ceil((double) piles[i]/mid);
         }
-        return hours <= h;
+        return count;
     }
 }
