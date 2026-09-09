db90007fd8f5694e92e9626b7b882e8149ecddde
Rachana D N <rachanadn27@gmail.com>
2026-09-09 15:56:28 +0530

    Time: 1 ms (99.36%) | Memory: 42.8 MB (37.18%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -5,11 +5,9 @@ class Solution {
         long rangeStart = 1000;
         long rangeEnd = rangeStart * 1000 - 1;
         int commas = 1;
-
         while (rangeStart <= n) {
             long numbers = Math.min(n, rangeEnd) - rangeStart + 1;
             totalCommas += (long) commas * numbers;
-
             if (rangeEnd > n) break;
 
             rangeStart = rangeStart * 1000;
