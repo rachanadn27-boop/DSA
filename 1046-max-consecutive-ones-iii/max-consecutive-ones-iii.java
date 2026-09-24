8d00ccc948ea0111233dcbde263faf10fd13f92d
Rachana D N <rachanadn27@gmail.com>
2026-09-24 21:30:48 +0530

    Time: 4 ms (44.48%) | Memory: 52 MB (73.55%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,8 +1,8 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
+        int max=0;
         int st=0;
         int zeros=0;
-        int maxlen=0;
         for(int end=0;end<nums.length;end++){
             if(nums[end]==0){
                 zeros++;
@@ -14,8 +14,8 @@ class Solution {
                 }
                 st++;
             }
-            maxlen=Math.max(maxlen,end-st+1);
+            max=Math.max(max,end-st+1);
         }
-        return maxlen;
+        return max;
     }
 }
