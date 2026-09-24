df7226d7dd707199f11db91408a1ccce9228596e
Rachana D N <rachanadn27@gmail.com>
2026-09-24 21:37:50 +0530

    Time: 3 ms (23.23%) | Memory: 64.7 MB (91.98%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -2,7 +2,6 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
 
         int n = nums.length;
-
         int[] left = new int[n];
         int[] right = new int[n];
         int[] ans = new int[n];
